f3e6f7920f0a74ba39fa12719a98ae3c623af791
YSHONG\ksh40 <qkdxks0922@gmail.com>
2022-11-10 11:26:53 +0900

    20221110 타임어택 제출

---
+++ user/views.py
@@ -0,0 +1,17 @@
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from user.serializers import UserSerializer, CustomTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"회원 가입이 완료되었습니다."}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message":f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
+    
+class CustomTokenObtainPairView(TokenObtainPairView):
+	serializer_class = CustomTokenObtainPairSerializer
